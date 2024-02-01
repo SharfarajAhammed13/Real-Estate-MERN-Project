33830b472ee29e1a9a6572be7f1320f6a00211d5
SHAIK SHARFARAJ AHAMMED <sharfaraj13@gmail.com>
2024-02-01 12:36:30 +0530

    added mongodb

--- api/index.js
+++ api/index.js
@@ -1,5 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv, { config } from "dotenv";
 
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('connected to mongoDB!');
+})
+.catch((err) => {
+    console.log(err);
+});
 const app = express();
 
 app.listen(3000, () => {
